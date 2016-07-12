80561eabdf330f00d7ee86275e7b8523e04aebf9
aileen santos <asantos3026@gmail.com>
2016-07-12 16:13:20 -0700

    Reorganized files, added nav bar

---
+++ js/app.js
@@ -0,0 +1,9 @@
+window.addEventListener( 'scroll', function( event ) {
+  var navBar = document.getElementsByTagName( 'nav' )[ 0 ];
+
+  if( window.pageYOffset > 0 ) {
+    navBar.classList.add( 'overlay' );
+  } else {
+    navBar.classList.remove( 'overlay' );
+  }
+});
